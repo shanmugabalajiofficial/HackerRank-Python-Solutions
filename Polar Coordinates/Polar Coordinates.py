32efa1da1eccab0557eb60abdf720fcc67302f90
shanmugabalajiofficial <shanmugabalajiofficial@gmail.com>
2024-05-16 14:21:53 +0530

    Create Polar Coordinates.py

---
+++ Polar Coordinates/Polar Coordinates.py
@@ -0,0 +1,8 @@
+#Polar Coordinates
+
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import cmath
+num = complex(input())
+print(cmath.polar(num)[0])
+print(cmath.polar(num)[1])
