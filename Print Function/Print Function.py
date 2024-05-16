179fb8ed5c1c81695c7ffb72726ee9568c498384
shanmugabalajiofficial <shanmugabalajiofficial@gmail.com>
2024-05-16 09:02:48 +0530

    Create Print Function.py

---
+++ Print Function/Print Function.py
@@ -0,0 +1,10 @@
+#Print Function
+
+
+if __name__ == '__main__':
+    n = int(input())
+for i in range(1, n+1):
+    print(i, end="")
+    
+    
+'''The end parameter in the print function is used to add any string. At the end of the output of the print statement in python. By default, the print function ends with a newline. Passing the whitespace to the end parameter (end=' ') indicates that the end character has to be identified by whitespace and not a newline.'''
