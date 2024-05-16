ebabd90e2636a0e20f6f17f18701771153976d62
shanmugabalajiofficial <shanmugabalajiofficial@gmail.com>
2024-05-16 14:32:00 +0530

    Create Mod Divmod.py

---
+++ Mod Divmod/Mod Divmod.py
@@ -0,0 +1,9 @@
+#Mod Divmod
+
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+fnum = int(input())
+snum = int(input())
+print(fnum // snum)
+print(fnum % snum)
+print(f"({fnum // snum}" + ", " + f"{fnum %snum})")
