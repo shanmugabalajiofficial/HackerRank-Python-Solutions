2c746ac906610a29051d7468a622701389e4dd3a
shanmugabalajiofficial <shanmugabalajiofficial@gmail.com>
2024-05-16 08:42:11 +0530

    Update String Validators.py

--- String Validators/String Validators.py
+++ String Validators/String Validators.py
@@ -1,3 +1,17 @@
 #String Validators
 
 
+if __name__ == '__main__':
+    s = input()
+
+a = any(char.isalnum()for char in s)
+b = any(char.isalpha()for char in s)
+c = any(char.isdigit()for char in s)
+d = any(char.islower()for char in s)
+e = any(char.isupper()for char in s)
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
