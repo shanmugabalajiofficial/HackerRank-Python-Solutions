ae07cfc4cd41851415b0c95e176e25f61bb5c63b
shanmugabalajiofficial <shanmugabalajiofficial@gmail.com>
2024-05-24 09:20:06 +0530

    Create Set .intersection() Operation.py

---
+++ Set .intersection() Operation/Set .intersection() Operation.py
@@ -0,0 +1,11 @@
+#Set .intersection() Operation
+
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+num_english = int(input())
+roll_num1 = set(map(int, input().split()))
+num_french = int(input())
+roll_num2 = set(map(int, input().split()))
+
+result = len(roll_num2.intersection(roll_num1))
+print(result)
