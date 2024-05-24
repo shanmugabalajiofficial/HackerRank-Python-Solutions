2465b8a9ba7ee54731c855ef5772635e1daf02f3
shanmugabalajiofficial <shanmugabalajiofficial@gmail.com>
2024-05-24 09:43:05 +0530

    Create Check Subset.py

---
+++ Check Subset/Check Subset.py
@@ -0,0 +1,11 @@
+#Check Subset
+
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+T=int(input())
+for i in range(T):
+    len_a=int(input())
+    a=set(map(int, input().split()))
+    len_b=int(input())
+    b=set(map(int, input().split()))
+    print(a.issubset(b))
