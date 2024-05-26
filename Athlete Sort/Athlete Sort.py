e9e896a81d51e9b7c21696a435a407f6680ec466
shanmugabalajiofficial <shanmugabalajiofficial@gmail.com>
2024-05-26 10:36:39 +0530

    Create Athlete Sort.py

---
+++ Athlete Sort/Athlete Sort.py
@@ -0,0 +1,35 @@
+#Athlete Sort
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def athelete_sort(arr, k):
+    
+    arr.sort(key=lambda x: x[k])
+    for e_lst in arr:
+        print(*e_lst)
+    
+
+
+
+if __name__ == '__main__':
+    nm = input().split()
+
+    n = int(nm[0])
+
+    m = int(nm[1])
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    k = int(input())
+    
+    athelete_sort(arr, k)
