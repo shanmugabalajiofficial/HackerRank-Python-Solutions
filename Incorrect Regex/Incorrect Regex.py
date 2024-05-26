62de700b668765cb2787956584c9f9a61a08f998
shanmugabalajiofficial <shanmugabalajiofficial@gmail.com>
2024-05-26 17:47:42 +0530

    Create Incorrect Regex.py

---
+++ Incorrect Regex/Incorrect Regex.py
@@ -0,0 +1,14 @@
+#Incorrect Regex
+
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
+
+n = int(input())
+
+for _ in range(n):
+    strings = input()
+    try:
+        print(bool(re.compile(strings)))
+    except re.error:
+        print('False')
