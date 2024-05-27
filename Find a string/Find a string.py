30dc0006a9aaac44b33758cac5b8380036fcb4a4
shanmugabalajiofficial <shanmugabalajiofficial@gmail.com>
2024-05-27 12:15:06 +0530

    Create Find a string.py

---
+++ Find a string/Find a string.py
@@ -0,0 +1,14 @@
+#Find a string
+
+
+import re
+def count_substring(string, sub_string):
+    num = len(re.findall(f'(?={sub_string})', string))
+    return num
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
